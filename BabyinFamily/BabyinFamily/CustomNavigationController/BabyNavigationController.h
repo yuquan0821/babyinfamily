16cad21716ed9ec7f08ed81b1916669247aff26d
minifans <mini-fans@hotmail.com>
2013-08-02 22:13:16 +0800

    comme back babynavigation

---
+++ BabyinFamily/BabyinFamily/CustomNavigationController/BabyNavigationController.h
@@ -0,0 +1,17 @@
+//
+//  BabyNavigationController.h
+//  BabyinFamily
+//
+//  Created by dong quan on 13-7-6.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+
+@interface BabyNavigationController : UINavigationController
+// Enable the drag to back interaction, Defalt is YES.
+@property (nonatomic,assign) BOOL canDragBack;
+
+@end
---
+++ BabyinFamily/BabyinFamily/CustomNavigationController/BabyNavigationController.m
@@ -0,0 +1,104 @@
+//
+//  BabyNavigationController.m
+//  BabyinFamily
+//
+//  Created by dong quan on 13-7-6.
+//
+//
+
+#import "BabyNavigationController.h"
+
+#define KEY_WINDOW  [[UIApplication sharedApplication]keyWindow]
+#define BackGesture_Mindistance  100
+@interface BabyNavigationController ()
+{
+    CGPoint startTouch;
+}
+@end
+
+@implementation BabyNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+
+        // Custom initialization
+        self.canDragBack = YES;
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    UIImage *navBackgroundImage = [UIImage imageNamed:@"header_bg"];
+    [[UINavigationBar appearance] setBackgroundImage:navBackgroundImage forBarMetrics:UIBarMetricsDefault];
+    
+    // Change UINavigationBar appearance by setting the font, color, shadow and offset.
+    [[UINavigationBar appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                          
+                                                          [UIColor colorWithRed:245.0 / 255.0 green:245.0 / 255.0 blue:245.0 / 255.0 alpha:1.0], UITextAttributeTextColor,
+                                                          
+                                                          [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8], UITextAttributeTextShadowColor,
+                                                          
+                                                          [NSValue valueWithUIOffset:UIOffsetMake(0, 1)], UITextAttributeTextShadowOffset,
+                                                          
+                                                          [UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:21.0], UITextAttributeFont,
+                                                          
+                                                          nil]];
+    
+    // Change the UIBarButtonItem apperance by setting a resizable background image for the back button.
+    UIImage *backButtonImage = [[UIImage imageNamed:@"navigationBarBackButton"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 13, 0, 6)];
+    [[UIBarButtonItem appearance] setBackButtonBackgroundImage:backButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    
+    // Change the UIBarButtonItem apperance by setting a resizable background image for the edit button.
+    UIEdgeInsets insets = {0, 6, 0, 6};// Same as doing this: UIEdgeInsetsMake (top, left, bottom, right)
+    UIImage *barButtonImage = [[UIImage imageNamed:@"button_normal"] resizableImageWithCapInsets:insets];
+    [[UIBarButtonItem appearance] setBackgroundImage:barButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    
+    UIPanGestureRecognizer *recognizer = [[[UIPanGestureRecognizer alloc]initWithTarget:self
+                                                                                 action:@selector(paningGestureReceive:)]autorelease];
+    [recognizer delaysTouchesBegan];
+    [self.view addGestureRecognizer:recognizer];
+}
+- (void)paningGestureReceive:(UIPanGestureRecognizer *)recoginzer
+{
+    // If the viewControllers has only one vc or disable the interaction, then return.
+    if (self.viewControllers.count <= 1 || !self.canDragBack) return;
+    
+    // we get the touch position by the window's coordinate
+    CGPoint touchPoint = [recoginzer locationInView:KEY_WINDOW];
+    
+    // begin paning, show the backgroundView(last screenshot),if not exist, create it.
+    if (recoginzer.state == UIGestureRecognizerStateBegan) {
+        startTouch = touchPoint;
+        
+    }else if (recoginzer.state == UIGestureRecognizerStateEnded){
+        
+        if (touchPoint.x - startTouch.x > BackGesture_Mindistance)
+        {
+            [self popViewControllerAnimated:YES];
+        }
+        else
+        {
+            
+        }
+        return;
+        
+        // cancal panning, alway move to left side automatically
+    }else if (recoginzer.state == UIGestureRecognizerStateCancelled){
+        
+        return;
+    }    
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
